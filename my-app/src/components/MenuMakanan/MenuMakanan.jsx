8a4d7ffe7de499635b923b4cdd0a6d193d377ff1
Valentina Resha <silvydwiap88@gmail.com>
2025-11-13 01:02:57 +0700

    function & class component

---
+++ my-app/src/components/MenuMakanan/MenuMakanan.jsx
@@ -0,0 +1,11 @@
+const MenuMakanan = () => {
+    return(
+        <div style={{ border: '1px solid black', width:300, margin:'auto' }}>
+            <p>Nama Menu: Ayam Bakar</p>
+            <p>Harga: Rp 25.000</p>
+        </div>
+    )
+    
+}
+
+export default MenuMakanan
---
+++ my-app/src/components/MenuMinuman/MenuMinuman.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class MenuMinuman extends Component{
+    render(){
+        return(
+            <div style={{ border: '1px solid black', width:300, margin:'auto' }}>
+                <p>Nama Menu: Juice Melon</p>
+                <p>Harga: Rp 15.000</p>
+            </div>
+        )
+    }
+}
+
+export default MenuMinuman
