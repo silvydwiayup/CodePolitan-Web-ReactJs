9def15974b866b8966b53eb12f77faf11c02361a
Valentina Resha <silvydwiap88@gmail.com>
2025-11-13 20:38:23 +0700

    react form

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -6,6 +6,9 @@ import MenuMinuman from './components/MenuMinuman/MenuMinuman';
 function App() {
 
   var [namaResto, setNamaResto] = useState('');
+  var [name, setName] = useState('')
+  var [Desc, setDesc] = useState('')
+  var [Payment, setPayment] = useState('')
   var data = "Cafe and Resto CodePolitan";
   var [namaMenu, setNamaMenu] = useState(
     {
@@ -31,12 +34,45 @@ function App() {
     setNamaResto(props);
   };
 
+  var handleOnChangeName = (event) => {
+    setName(event.target.value);
+  };
+
+  var handleOnChangeDesc = (event) => {
+    setDesc(event.target.value);
+  };
+  
+  var handleOnChangePayment = (event) => {
+    setPayment(event.target.value);
+  };
+
+
 
   return (
     <>
       <div className="App">
         <h1>{namaResto}</h1>
         <button onClick={() => handleGantiResto ('Programming')}>Ganti Nama Resto</button>
+
+        <form>
+          <label style={{ marginTop: '20px', display: "block" }}>Nama</label>
+          <input style={{ width: '80%', minHeight: '30px' }}  type="text"  name="inputNama" onChange={handleOnChangeName}/>
+          <p>Nama : {name} </p>
+
+          <label style={{ display: "block" }}>Keterangan</label>
+          <textarea style={{ width: '80%', minHeight: '80px' }} name="inputKeterangan" onChange={handleOnChangeDesc}></textarea>
+          <p>Keterangan : {Desc} </p>
+
+          <label style={{ display: "block" }}>Pembayaran</label>
+          <select style={{ width: '80%', minHeight: '30px' }} name="inputPayment" onChange={handleOnChangePayment}>
+            <option value="Cash">Cash</option>
+            <option value="Debit">Debit</option>
+            <option value="Voucher">Voucher</option>
+          </select>
+          <p>Pembayaran : {Payment} </p>
+        </form>
+        
+
         <h2>Menu Makanan</h2>
         <MenuMakanan namaMenu={namaMenu.makanan[0].namaMakanan} hargaMenu={namaMenu.makanan[0].hargaMenu} stockMenu= {namaMenu.makanan[0].stock}/> <br />
         <MenuMakanan namaMenu={namaMenu.makanan[1].namaMakanan} hargaMenu={namaMenu.makanan[1].hargaMenu} stockMenu= {namaMenu.makanan[1].stock}/>
