9fe749022c93da4343a2bdadc20956359e90a52e
Valentina Resha <silvydwiap88@gmail.com>
2025-11-15 23:35:38 +0700

    instalasi react-router

---
+++ codepolitan/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+
+      </div>
+    </>
+  )
+}
+
+export default App
